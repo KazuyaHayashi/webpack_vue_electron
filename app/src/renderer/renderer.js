eca83809485f69aff045b2802affab033d8139fb
Kazuya Hayashi <pumpkin.brownies@gmail.com>
2018-11-27 17:01:12 +0900

    todo list sample

--- app/src/renderer/renderer.js
+++ app/src/renderer/renderer.js
@@ -1,8 +1,17 @@
 import Vue from 'vue';
 
-new Vue({
+Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+});
+
+let app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    }
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    },
 });
