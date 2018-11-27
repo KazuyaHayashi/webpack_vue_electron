920f35850b8ad8814d24580912212f011d9d62fc
Kazuya Hayashi <pumpkin.brownies@gmail.com>
2018-11-27 18:13:20 +0900

    use bootstrap-vue

--- app/src/renderer/renderer.js
+++ app/src/renderer/renderer.js
@@ -1,11 +1,11 @@
 import Vue from 'vue';
 import HelloComponent from './components/hello.vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.component('todo-item', {
-    props: ['todo'],
-    template: '<li>{{ todo.text }}</li>'
-});
 
+Vue.use(BootstrapVue);
 let app = new Vue({
     el: '#app',
     components: {'hello': HelloComponent},
